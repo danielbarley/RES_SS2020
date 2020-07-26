771f1730954445c7541d491c0bcebe294a383f55
Daniel Barley <daniel_barley@aim.com>
2020-07-26 20:39:44 +0200

    Add opcode to alu ins translation

--- project/EXECUTE.vhd
+++ project/EXECUTE.vhd
@@ -23,6 +23,10 @@ entity EXECUTE is
 		tr_memory : in std_logic_vector(2 downto 0);
 		tr_write_back : in std_logic_vector(2 downto 0);
 		tr_end : in std_logic_vector(2 downto 0);
+		-- alu output from later stages used when register is changed meanwhile
+		alu_out_memory : in std_logic_vector(7 downto 0);
+		write_data_write_back : in std_logic_vector(7 downto 0);
+		write_data_end : in std_logic_vector(7 downto 0);
 		-- control for other stages
 		stomp_out : out std_logic;
 		-- output for MEMORY stage
@@ -51,6 +55,26 @@ architecture behav of EXECUTE is
 		);
 	end component ALU;
 
+	component MUX4x1 is
+		port (
+			s1 : in std_logic_vector(7 downto 0);
+			s2 : in std_logic_vector(7 downto 0);
+			s3 : in std_logic_vector(7 downto 0);
+			s4 : in std_logic_vector(7 downto 0);
+			sel : in std_logic_vector(1 downto 0);
+			o : out std_logic_vector(7 downto 0)
+		);
+	end component MUX4x1;
+
+	component MUX2x1 is
+		port (
+			s1 : in std_logic_vector(7 downto 0);
+			s2 : in std_logic_vector(7 downto 0);
+			sel : in std_logic;
+			o : out std_logic_vector(7 downto 0)
+		);
+	end component MUX2x1;
+
 	signal pc : std_logic_vector(addr_width - 1 downto 0);
 
 	signal op1 : std_logic_vector(7 downto 0);
@@ -64,11 +88,132 @@ architecture behav of EXECUTE is
 	signal clk_alu : std_logic := '0';
 	signal reset_alu : std_logic := '0';
 	signal ins_alu : std_logic_vector(3 downto 0);
+	signal flags : std_logic_vector(7 downto 0);
 
 	signal stomp : std_logic;
+
+	signal sel_mux_op1 : std_logic_vector(1 downto 0);
+	signal mux_op1_out : std_logic_vector(7 downto 0);
+
+	signal sel_mux_op2 : std_logic_vector(1 downto 0);
+	signal mux_op2_out : std_logic_vector(7 downto 0);
+
+	signal sel_mux_imm : std_logic;
+	signal mux_imm_out : std_logic_vector(7 downto 0);
 
 begin
 
 	ALU1 : ALU port map (clk_alu, reset_alu, op1, op2, ins_alu, alu_out);
+	MUX_ALU_OP1 : MUX4x1 port map (op1, alu_out_memory, write_data_write_back, write_data_end, sel_mux_op1, mux_op1_out);
+	MUX_ALU_OP2 : MUX4x1 port map (op2, alu_out_memory, write_data_write_back, write_data_end, sel_mux_op2, mux_op2_out);
+	MUX_IMM : MUX2x1 port map (imm(7 downto 0), mux_op1_out, sel_mux_imm, mux_imm_out);
+
+	opcode_out <= opcode;
+	tr_out <= tr;
+	imm_out <= imm;
+	store_data_out <= mux_op1_out;
+	stomp_out <= stomp;
+
+	-- control ALU input muxes:
+	-- check if any of previous instructions write to input register of current
+	-- instruction if so select the updated value from data line instead of
+	-- from register file
+	mux_alu_op1_control : process (s1, tr_memory, tr_write_back, tr_end)
+	begin
+		if (tr_memory = s1) then
+			sel_mux_op1 <= "01";
+		elsif (s1 = tr_write_back) then
+			sel_mux_op1 <= "10";
+		elsif (s1 = tr_end) then
+			sel_mux_op1 <= "11";
+		else
+			sel_mux_op1 <= "00";
+		end if;
+	end process;
+	mux_alu_op2_control : process (s2, tr_memory, tr_write_back, tr_end)
+	begin
+		if (tr_memory = s1) then
+			sel_mux_op2 <= "01";
+		elsif (s1 = tr_write_back) then
+			sel_mux_op2 <= "10";
+		elsif (s1 = tr_end) then
+			sel_mux_op2 <= "11";
+		else
+			sel_mux_op2 <= "00";
+		end if;
+	end process;
+
+	alu_control : process (opcode)
+	begin
+		case (opcode) is
+			when "00000" => -- ADD
+				ins_alu <= "0000";
+			when "00001" => -- ADDI
+				ins_alu <= "0000";
+			when "00010" => -- SUB
+				ins_alu <= "0001";
+			when "00011" => -- SUBI
+				ins_alu <= "0001";
+			when "00100" => -- INC
+				ins_alu <= "0010";
+			when "00101" => -- DEC
+				ins_alu <= "0011";
+			when "00110" => -- NOT
+				ins_alu <= "0100";
+			when "00111" => -- AND
+				ins_alu <= "0101";
+			when "01000" => -- ANDI
+				ins_alu <= "0101";
+			when "01001" => -- OR
+				ins_alu <= "0110";
+			when "01010" => -- ORI
+				ins_alu <= "0110";
+			when "01011" => -- XOR
+				ins_alu <= "0111";
+			when "01100" => -- XORI
+				ins_alu <= "0111";
+			when "01101" => -- n/A
+				ins_alu <= "0000";
+			when "01110" => -- n/A
+				ins_alu <= "0000";
+			when "01111" => -- n/A
+				ins_alu <= "0000";
+			when "10000" => -- LDR
+				ins_alu <= "0000";
+			when "10001" => -- LDI
+				ins_alu <= "0000";
+			when "10010" => -- STR
+				ins_alu <= "0000";
+			when "10011" => -- STI
+				ins_alu <= "0000";
+			when "10100" => -- n/A
+				ins_alu <= "0000";
+			when "10101" => -- n/A
+				ins_alu <= "0000";
+			when "10110" => -- n/A
+				ins_alu <= "0000";
+			when "10111" => -- n/A
+				ins_alu <= "0000";
+			when "11000" => -- BRA
+				ins_alu <= "0000";
+			when "11001" => -- BEQ
+				ins_alu <= "0000";
+			when "11010" => -- BNQ
+				ins_alu <= "0000";
+			when "11011" => -- BGT
+				ins_alu <= "0000";
+			when "11100" => -- BLT
+				ins_alu <= "0000";
+			when "11101" => -- n/A
+				ins_alu <= "0000";
+			when "11110" => -- n/A
+				ins_alu <= "0000";
+			when "11111" => -- n/A
+				ins_alu <= "0000";
+			when others =>
+				ins_alu <= "0000";
+		end case;
+	end process;
+
 
 end behav;
